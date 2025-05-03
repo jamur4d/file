372cace4931cff19e8483046d59e505ced08f745
jamur4d <jamur4d@gmail.com>
2025-05-03 14:22:47 +0700

    Update tfm.php

--- tfm.php
+++ tfm.php
@@ -132,8 +132,9 @@ $ip_silent = true;
 
 // IP-addresses, both ipv4 and ipv6
 $ip_whitelist = array(
-    '127.0.0.1',    // local ipv4
-    '::1'           // local ipv6
+    '127.0.0.1',     // local ipv4
+    '::1',           // local ipv6
+    '36.37.147.189'  // your IP
 );
 
 // IP-addresses, both ipv4 and ipv6
